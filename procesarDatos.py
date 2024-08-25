2b7909cd8076e6dfa47f1616a4371b2d60055c7e
Huron_16 <132000694+Huroncito16@users.noreply.github.com>
2024-08-25 10:42:43 -0600

    la curtosis no me da XD

--- procesarDatos.py
+++ procesarDatos.py
@@ -1,20 +1,10 @@
 from collections import Counter
+import math
 from readExcel import leerDatos
 
 # reemplazar esto para que tome los datos de tu lista 
 # estesolo es prueba
-# datos = leerDatos()
-
-datos = [ 11, 9,	8,	7,	11,	6,	11,
-11,	11,	8,	6,	9,	7,	10,
-11,	5,	9,	7,	8,	6,	5,
-9,	7,	5,	5,	6,	8,	8,
-8,	9,	11,	8,	8,	8,	10,
-10,	8,	9,	7,	9,	6,	7,
-9,	9,	11,	10,	7,	10,	8,
-11,	10,	5,	5,	5,	6,	10,
-6,	6,	10,	7,	9,	9,	10,
-8,	7,	6,	10,	7,	9,	7]
+datos = leerDatos()
 
 frecuencias = Counter(datos)
 n = len(datos)
@@ -36,11 +26,11 @@ frecuencia_abs_delta_cuarta = []
 
 fa_acum = 0
 fd_acum = n
-
 media = sum([valor * frecuencias[valor] for valor in valores]) / n
 
 for valor in valores:
     f = frecuencias[valor]
+    d = valor - media
     fa_acum += f
     fr = (f / n) * 100
     fr_acum_rel = (fa_acum / n) * 100
@@ -97,23 +87,27 @@ print(f"{'T':<5}{total_f:<5}{total_f_rel:<7.2f}{'':<19}"
       f"{total_delta_cuarta:<7.2f}")
 
 # Resumen estadístico
+minimo = min(datos)
+maximo = max(datos)
+rango = max(datos) - min(datos)
 media = sum([x * f for x, f in frecuencias.items()]) / n
 mediana = sorted(datos)[n // 2] if n % 2 != 0 else (sorted(datos)[n // 2 - 1] + sorted(datos)[n // 2]) / 2
 moda = max(frecuencias, key=frecuencias.get)
 varianza = total_delta_cuadrado / n
+desviacion_estandar = math.sqrt(varianza)
 curtosis = (n * total_delta_cuarta) / (total_delta_cuadrado ** 2) - 3
 coef_asimetria = (n * total_delta_cubo) / (total_delta_cuadrado ** (3/2))
-rango = max(datos) - min(datos)
-minimo = min(datos)
-maximo = max(datos)
+
 
 print("\nResumen Estadístico")
+print("-" * 35)
+print(f"{'Valor Mínimo':<20}: {minimo}")
+print(f"{'Valor Máximo':<20}: {maximo}")
+print(f"{'Rango':<20}: {rango}")
 print(f"{'Media':<20}: {media:.2f}")
 print(f"{'Mediana':<20}: {mediana:.2f}")
 print(f"{'Moda':<20}: {moda}")
 print(f"{'Varianza':<20}: {varianza:.2f}")
+print(f"{'Desviación Estándar':<20}: {desviacion_estandar:.2f}")
 print(f"{'Curtosis':<20}: {curtosis:.2f}")
 print(f"{'Coef. Asimetría':<20}: {coef_asimetria:.2f}")
-print(f"{'Rango':<20}: {rango}")
-print(f"{'Valor Mínimo':<20}: {minimo}")
-print(f"{'Valor Máximo':<20}: {maximo}")
